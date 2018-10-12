ef48b8a1848c9a808f3eddea5d9a29c50777c462
candicevv <ytchen103@163.com>
2018-10-12 15:34:47 +0800

    Update and rename NPL.R to t-SNE.R

--- t-SNE.R
+++ t-SNE.R
@@ -1,33 +1,35 @@
 library(readxl)
 
-NPL <- read_excel("C:/Users/Chen Validus Team/inSync Share/Data Tech/NPL Study/NPL Study feature.xlsx", 
-                  sheet = "Sheet1")
-sample_data<-NPL[,c(1,3:10,12:14,16:19,22:24)]
+raw <- read_excel(myfile, sheet = "Sheet1")
+sample_data<-raw[,c(1,3:10,12:14,16:19,22:24)]
+# remove cases with null or error value
 sample_data<-sample_data[complete.cases(sample_data), ]
 
 library(caret)  
 library(Rtsne)
 
+#for plot label
 addline_format <- function(x,...){ 
   gsub('\\s','\n',x) 
 } 
 
-
 ## Rtsne function may take some minutes to complete...
-set.seed(9)  
-tsne_model_1 = Rtsne(as.matrix(scale(sample_data[,-c(1,2,6,7,8,9,11,19)])), check_duplicates=FALSE, 
+set.seed(100)  
+tsne_model_1 = Rtsne(as.matrix(scale(sample_data[,-1])), check_duplicates=FALSE, 
                      pca=FALSE, perplexity=10, theta=0.5, dims=2)
 
 ## getting the two dimension matrix
 d_tsne_1 = as.data.frame(tsne_model_1$Y)
 d_tsne_1<-cbind(d_tsne_1,sample_data$Label,sample_data$`Borrower Name`)
 colnames(d_tsne_1)<-c("V1","V2","Label","Borrower_Name")
+
 ## plotting the results without clustering
+#Set label name with add_line function to avoid name missing in plot
 ggplot(d_tsne_1, aes(x=V1, y=V2,color=as.factor(Label),label=addline_format(Borrower_Name))) +  
   geom_point(size=5) +
   guides(colour=guide_legend(title="Status",override.aes=list(size=4))) +
   xlab("") + ylab("") +
-  ggtitle("t-SNE clustering for existing Borrowers") +
+  ggtitle("t-SNE clustering") +
   geom_text(angle=0, size=2)+
   theme_light(base_size=15) +
   theme(axis.text.x=element_blank(),
@@ -42,13 +44,14 @@ ggplot(d_tsne_1, aes(x=V1, y=V2,color=as.factor(Label),label=addline_format(Borr
 d_tsne_1_original=d_tsne_1
 
 ## Creating k-means clustering model, and assigning the result to the data used to create the tsne
+#clustering model would perform better if data is scaled.
 fit_cluster_kmeans=kmeans(scale(d_tsne_1[,c(1,2)]), 5)  
 d_tsne_1_original$cl_kmeans = factor(fit_cluster_kmeans$cluster)
 
 ## Creating hierarchical cluster model, and assigning the result to the data used to create the tsne
 fit_cluster_hierarchical=hclust(dist(scale(d_tsne_1[,c(1,2)])))
 
-## setting 3 clusters as output
+## setting 5 clusters as output
 d_tsne_1_original$cl_hierarchical = factor(cutree(fit_cluster_hierarchical, k=5))
 
 plot_cluster=function(data, var_cluster, palette)  
